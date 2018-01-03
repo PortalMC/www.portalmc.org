732b89e505098c2475f05bfe846bc96f338a4e8a
Ralph <git@ralph.ms>
2018-01-03 01:31:31 +0000

    [ci skip] 2018/01/03 01:31:31

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -1,1 +1,36 @@
-base_url = window.location.protocol + "//" + window.location.host;
+(function () {
+    var current_language_prefix = document.querySelector("html").getAttribute("data-path-prefix");
+
+    // Radio button
+    var radioDoms = document.querySelectorAll('.mdc-radio');
+    var radios = [];
+    for (var i = 0, radioDom; radioDom = radioDoms[i]; i++) {
+        radios[i] = new mdc.radio.MDCRadio(radioDom);
+    }
+
+    // Dialog
+    var dialog = new mdc.dialog.MDCDialog(document.querySelector('#dialog-language-trigger'));
+    dialog.listen('MDCDialog:accept', function () {
+        for (var i = 0, radio; radio = radios[i]; i++) {
+            var prefix = radioDoms[i].getAttribute("data-path-prefix");
+            if (prefix === current_language_prefix) {
+                continue;
+            }
+            if (radio.checked) {
+                changeLanguage(prefix)
+            }
+        }
+    });
+    document.querySelector('#language-trigger').addEventListener('click', function (evt) {
+        evt.preventDefault();
+        dialog.lastFocusedTarget = evt.target;
+        for (var i = 0, radio; radio = radios[i]; i++) {
+            radio.checked = radioDoms[i].getAttribute("data-path-prefix") === current_language_prefix;
+        }
+        dialog.show();
+    });
+
+    function changeLanguage(prefix) {
+        window.location.href = window.location.protocol + "//" + window.location.host + prefix
+    }
+})();
